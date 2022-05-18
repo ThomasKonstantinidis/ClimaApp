6e1370acfdafb6b29885a69fb2d6051563213bc8
Thomaskonstantinidis <konidis.tom@gmail.com>
2022-05-18 21:53:46 +0300

    Width_Height changes

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.project;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.CountDownTimer;
+import android.view.Gravity;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -46,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
         this.airTypeButton = (Button)findViewById(R.id.AirType);
         this.airIntensityButton = (Button)findViewById(R.id.AirIntensity);
 
-
     }
 
     @Override
@@ -71,4 +75,46 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void onBackPressed(){
+        new AlertDialog.Builder(this)
+                .setTitle("Έξοδος: ")
+                .setMessage("Θέλετε να απενεργοποιήσετε το κλιματιστικό;")
+                .setNegativeButton("Όχι", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        on = true;
+                    }
+                })
+                .setPositiveButton("Ναι", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        goodbye();
+                    }
+                }).create().show();
+    }
+
+    private void goodbye(){
+        Context context = this;
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+        TextView message = new TextView(context);
+        message.setText("Το κλιματιστικό \nαπενεργοποιήθηκε");
+        message.setGravity(Gravity.CENTER_HORIZONTAL);
+        alertDialogBuilder.setView(message);
+
+
+
+        final AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+
+        new CountDownTimer( 1000, 1000){
+            @Override
+            public void onTick(long l) {}
+
+            @Override
+            public void onFinish() {
+                alert.dismiss();
+            }
+        }.start();
+    }
 }
